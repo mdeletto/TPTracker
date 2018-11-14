de619c62f78ab58e1adacac866e52df0d0bd5dc8
Michael D'Eletto <mikedeletto@gmail.com>
2018-11-14 14:09:33 -0500

    TS runs upload path fix

--- bin/pull_TS_images.sh
+++ bin/pull_TS_images.sh
@@ -2,5 +2,5 @@
 
 for server in 10.71.60.151 10.71.60.152
 do
-	rsync -av --ignore-existing --exclude "block*" --include "*/" --include "*.png" --exclude "*" ionadmin@$server:/results/analysis/output/Home/ /var/www/TPL/TPTracker/images
+	rsync -av --ignore-existing --exclude "block*" --include "*/" --include "*.png" --exclude "*" ionadmin@$server:/results/analysis/output/Home/ /var/www/html/TPTracker/images/TSruns
 done
