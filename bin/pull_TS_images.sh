e8d48400fd364b52d48ee04ec89436b26f4029e9
Michael D'Eletto <mikedeletto@gmail.com>
2018-11-14 14:06:20 -0500

    fixed TS server IPs

--- bin/pull_TS_images.sh
+++ bin/pull_TS_images.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for server in 10.80.61.55 10.80.157.20
+for server in 10.71.60.151 10.71.60.152
 do
 	rsync -av --ignore-existing --exclude "block*" --include "*/" --include "*.png" --exclude "*" ionadmin@$server:/results/analysis/output/Home/ /var/www/TPL/TPTracker/images
 done
